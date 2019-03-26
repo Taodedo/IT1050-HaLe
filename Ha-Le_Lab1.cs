6db3bb36f267382402131fab69fac832c444809c
Taodedo <s00828588@acad.tri-c.edu>
2019-03-25 21:48:37 -0400

    Add files via upload

---
+++ Ha-Le_Lab1.cs
@@ -0,0 +1,19 @@
+﻿// A Hello World From Ha Le program in C#.
+using System;
+
+namespace HelloWorld
+{
+	class Hello
+	{
+		static void Main()
+		{
+			String TheGreeting = "Hello World";
+			String MyName = "Ha Le";
+
+			Console.WriteLine($"{TheGreeting} From  {MyName}");
+			//Kep the console window open in debug mode.
+
+			Console.ReadKey();
+		}
+	}
+}
